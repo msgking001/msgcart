b23c930b7dea92cecbd4deb75c3fefae2c00ffb8
msgking001 <matheshking10@gmail.com>
2025-12-19 14:36:06 +0530

    Components updations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Header from './components/layouts/Header';
+import Footer from './components/layouts/footer';
+import Home from './components/layouts/home';
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom"
+import {HelmetProvider} from 'react-helmet-async'
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelmetProvider>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+    <Footer/>
+    </HelmetProvider>
+     
     </div>
+    </Router>
   );
 }
 
